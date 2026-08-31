8eb17cb3e0514f5a687b66cb404425df69b96883
XPMAGNETO <xpm69420@gmail.com>
2026-08-31 11:15:00 -0400

    feat: include Android APK in tagged releases

--- src/AutoEmulatorUpdate.Mobile.Android/MainActivity.cs
+++ src/AutoEmulatorUpdate.Mobile.Android/MainActivity.cs
@@ -7,6 +7,7 @@ namespace AutoEmulatorUpdate.Mobile.Android;
 [Activity(
     Label = "Auto Emulator Updater",
     Theme = "@style/MyTheme.NoActionBar",
+    Icon = "@drawable/app_icon",
     MainLauncher = true,
     ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
 public sealed class MainActivity : AvaloniaMainActivity;
